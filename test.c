260c3fbccddedabe60d0fa8e2d8ef8bb4b27b3f9
Me-Kiwi <atharvanalat0@gmail.com>
2024-05-08 18:55:05 +0530

    process CSI inputs

--- test.c
+++ test.c
@@ -5,6 +5,45 @@
 #include <sys/ioctl.h>
 #include <stdbool.h>
 
+
+ typedef enum{
+        OTHER,
+        UP_ARROW,
+        DOWN_ARROW,
+        RIGHT_ARROW,
+        LEFT_ARROW,
+    }special_code ;
+
+int  read_special_code(){
+    char code1 ;
+    char code2 ;
+    char code3 ;
+    special_code  special_code ;
+    read(STDIN_FILENO, &code1, 1);
+    printf("%c",code1) ;
+    if(code1 >= 0 && code1 < 10){
+        read(STDIN_FILENO, &code2, 1);
+        printf("%c", code2) ;   
+    }
+    read(STDIN_FILENO, &code3, 1) ;
+    printf("%c", code3) ;        
+    switch(code3){
+        case 'a' :
+            special_code = UP_ARROW ;
+            break ;
+        case 'b' :
+            special_code = DOWN_ARROW ;
+            break ;
+        case 'c' :
+            special_code = RIGHT_ARROW ;
+            break ;
+        case 'd' :
+            special_code = LEFT_ARROW ; 
+            break ;
+    }
+    return special_code ;
+}
+
 void enable_raw_mode() {
     struct termios raw;
     tcgetattr(STDIN_FILENO, &raw);
@@ -19,17 +58,6 @@ void disable_raw_mode(struct termios* original_termios) {
 void move_cursor_back(int rows, int cols) {
     printf("\033[%dA\033[%dD", rows, cols); 
 }
-
-typedef enum  {
-    UP_ARROW,
-    DOWN_ARROW,
-    RIGHT_ARROW,
-    LEFT_ARROW,
-} special_code;
-
-// special_code read_special_code() {
-    
-// }
 
 int main() {
     struct termios original_termios;
@@ -48,28 +76,20 @@ int main() {
     char** g = malloc(sizeof(char*)*10000);
     int f;
     char c;
+    int i = 0 ;
     while(true) {
         read(STDIN_FILENO, &c, 1); 
-        printf("%c", c);
-
-        if(c == '\0') {
-            read_speacial_code();
+        // printf("%d", i++) ;
+        printf("%c", c) ;
+        
+        if(c == 27) {
+            read_special_code();
+        }        fflush(stdout);
         }
-        
-        // if(c == "upprow") { g--; };
-        // if(c == "right") { f++; };
-        // writeline() { putcursorlinstart; printf(line); flush; }
-        // else if(c is ascii) { (*g)[f] = c; writeline((*g)) }
-        
-        fflush(stdout);
-    }
 
     fflush(stdout);
     disable_raw_mode(&original_termios);
 
     return 0;
 }
-    // printf("\033[6n"); // Request cursor position
-    // int row, col;
-    // scanf("\033[%d;%dR", &row, &col);
-    // printf("GOT %d %d\n", row, col);
+   
