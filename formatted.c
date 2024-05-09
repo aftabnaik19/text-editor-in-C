75cf6082c59c3231b6914390596094637e7203fb
Me-Kiwi <atharvanalat0@gmail.com>
2024-05-10 02:36:47 +0530

    formatted and organized code

--- formatted.c
+++ formatted.c
@@ -25,18 +25,30 @@ typedef struct row_state{
 
 typedef struct editorState {
   int fileposition_x, fileposition_y ;
-  int cursorposition_x, cursorposition_y ;
+  int offset_x, offset_y ;
   int screen_rows ;
   int screen_cols ;
   int no_of_lines ;
   FILE* fp ; 
 }editorState ;
 
+
+int min(int a, int b) {
+    return (a < b) ? a : b;
+}
+
+void clear_display(){
+  printf("\e[2J") ;
+}
+
+void cursor_to(int y,int x){
+  printf("\e[%d;%dH", y, x);
+}
+
 escseq CSI_code(){
   char ch ; 
   escseq escseq ;
   read(STDIN_FILENO, &ch, 1) ;//ignore
-  // if(ch > 0 && ch <9) for(int i=0;i<ch;i++,read(STDIN_FILENO, &ch, 1));
   read(STDIN_FILENO, &ch, 1) ;
   switch(ch){
     case 'A' : return UP ;
@@ -48,30 +60,22 @@ escseq CSI_code(){
  
 }
 
-int min(int a, int b) {
-    return (a < b) ? a : b;
-}
-void nprintf(row_state* r_state){
-  for(int i = 0 ; i < r_state->no_of_char ; i++){
-    printf("%c", r_state->line[i]) ;
-    fflush(stdout) ;
+void handle_CSI(editorState* State, escseq key) {
+  switch(key) {
+    case UP:
+      // TODO: add bound chekcing
+      State->fileposition_y--;
+      break;
+    case DOWN:
+      State->fileposition_y++;
+      break;
+    case RIGHT:
+      State->fileposition_x++;
+      break;
+    case LEFT:
+      State->fileposition_x--;
+      break;
   }
-  printf("\n") ;
-  fflush(stdout) ;
-}
-
-bool refresh_screen(editorState State, row_state* r_state, int bound ){
-  printf("\e[2J") ;
-  printf("\e[2;1H") ;//added space for header
-  fflush(stdout) ;
-  for(int i = 0 ; i < bound ; i++){
-    nprintf(r_state+i) ;
-    fflush(stdout);
-  }
-  // printf("UPDATED CURSOR TO %d %d",State.cursorposition_y, State.cursorposition_x );
-  printf("\e[%d;%dH", State.cursorposition_y+1, State.cursorposition_x+1);
-  fflush(stdout);
-  return 0 ;
 }
 
 void backSpace(editorState* State , row_state *r_state){
@@ -85,43 +89,10 @@ void backSpace(editorState* State , row_state *r_state){
   }
   r_state->no_of_char--;
   State->fileposition_x--;
-  State->cursorposition_x--;
 }
 
-// void memory_alloc_char(row_state* r_state){
-//   line = malloc(100);//realloc(r_state->line, sizeof(char)*(r_state->no_of_char+1));
-// }
-
-// void memory_alloc_line(editorState* State, row_state* r_state){
-//   r_state = malloc(r_state, sizeof(row_state)*count+1);
-//   (r_state + State->no_of_lines)->line = malloc(1000);
-// }
-
-void handle_CSI(editorState* State, escseq key) {
-  switch(key) {
-    case UP:
-      // TODO: add bound chekcing
-      State->fileposition_y--;
-      State->cursorposition_y--;
-      break;
-    case DOWN:
-      State->fileposition_y++;
-      State->cursorposition_y++;
-      break;
-    case RIGHT:
-      State->fileposition_x++;
-      State->cursorposition_x++;
-      break;
-    case LEFT:
-      State->fileposition_x--;
-      State->cursorposition_x--;
-      break;
-  }
-}
-// SPECIAL KEY CSI
 bool save_buffer(editorState* State, row_state* r_state){
-    //27 special code
-    
+  
     char input;
     read(STDIN_FILENO, &input, 1);
     switch(input) {
@@ -134,7 +105,7 @@ bool save_buffer(editorState* State, row_state* r_state){
         break;
       case '\n':
       
-        int pos_y = ++(State->cursorposition_y);
+        int pos_y = 1+(State->fileposition_y);
         int pos_x = (State->fileposition_x);
         State->fileposition_y++;
 
@@ -156,22 +127,46 @@ bool save_buffer(editorState* State, row_state* r_state){
         // Update the state
         State->no_of_lines++;
         State->fileposition_x= 0;
-        State->cursorposition_x=0;
       
         break;
       default: 
          for(int i=89; i>=State->fileposition_x+1; i--) {
              r_state[State->fileposition_y].line[i] = r_state[State->fileposition_y].line[i-1];
           }
-      if(isalnum(input) || isspace(input)) {
-        // memory_alloc_char(r_state+State->fileposition_y);
         r_state[State->fileposition_y].line[State->fileposition_x] = input;
-        State->cursorposition_x++;
         State->fileposition_x++;
         r_state[State->fileposition_y].no_of_char++;
-      }
     }
     return 1 ;
+}
+
+void nprintf(row_state* r_state){
+  for(int i = 0 ; i < r_state->no_of_char ; i++){
+    printf("%c", r_state->line[i]) ;
+    fflush(stdout) ;
+  }
+  printf("\n") ;
+  fflush(stdout) ;
+}
+
+bool refresh_screen(editorState State, row_state* r_state, int bound ){
+  clear_display() ;
+  printf("\e[2;1H") ;//added space for header
+  fflush(stdout) ;
+  for(int i = 0 ; i < bound ; i++){
+    nprintf(r_state+i) ;
+    fflush(stdout);
+  }
+  cursor_to(State.fileposition_y+2,State.fileposition_x+1) ;
+  fflush(stdout);
+  return 0 ;
+}
+
+void enable_raw_mode() {
+    struct termios raw;
+    tcgetattr(STDIN_FILENO, &raw);
+    raw.c_lflag &= ~(ECHO | ICANON);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 void get_window(int* rows, int* cols){
@@ -183,41 +178,26 @@ void get_window(int* rows, int* cols){
     *cols = ws.ws_col ;
 }
 
-void enable_raw_mode() {
-    struct termios raw;
-    tcgetattr(STDIN_FILENO, &raw);
-    raw.c_lflag &= ~(ECHO | ICANON);
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
-}
-
-
-void disable_raw_mode(struct termios* original_termios) {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, original_termios);
-}
-
-
 void initEditor(editorState* State){
   State->fileposition_x = 0 ;
   State->fileposition_y = 0 ;
-  State->cursorposition_x = 0 ;
-  State->cursorposition_y = 0 ;
+  State->offset_x = 0 ; 
+  State->offset_y = 0 ;
+  State->no_of_lines = 1 ;;
   get_window (&State->screen_rows, &State->screen_cols) ;
   //load the file
 }
 
-
 int main(int argc,char* argv[]){
   editorState State ;
-  // row_state *r_state = calloc(100, sizeof(row_state));
   row_state *r_state = malloc(sizeof(row_state)*10);
   r_state->line = malloc(sizeof(char)*1000);
   r_state->line_no = 1;
   bool changeflag = 0 ;
   enable_raw_mode() ;
   initEditor(&State) ;
-  State.no_of_lines=1;
   while(1){
-    char ch;
+    if(changeflag)
       changeflag = refresh_screen(State ,r_state, State.no_of_lines) ;
       changeflag = save_buffer(&State,r_state) ;
   }
